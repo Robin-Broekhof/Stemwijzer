7d9fbe1c0baa09ec0f4cdb27a8321d0f72adcced
Ye Xiu <99059076@mydavinci.nl>
2021-03-03 11:04:11 +0100

    antwoorden opgeslaan
    functie dat de vragen worden opgeslagen in een array,

--- resources/script.js
+++ resources/script.js
@@ -1,14 +1,10 @@
-let savedAnswers = []
+var savedAnswers = []
 
+var vraagcounter = 0
+var vraagnummer = 2
+console.log (subjects[vraagnummer].statement)
+console.log (subjects[vraagnummer].parties[1].position)
 
-let fruit = [
-    {
-        "name": "appel"
-    },
-    {
-        "name": "banaan"
-    }
-];
 
 
 
@@ -17,23 +13,40 @@ function homepage(){
     document.getElementById("onclick-button").style.display = ""
     document.getElementById("questions-content").style.display = "none"
 
-
-    document.getElementById("start").onclick = questionsPage
-
-
-
-    
+    document.getElementById("start").onclick = questiondom
 }
 
-function questionsPage(){
+function questiondom(){
     document.getElementById("homepage-content").style.display = "none"
     document.getElementById("onclick-button").style.display = "none"
     document.getElementById("questions-content").style.display = ""
 
-    document.getElementById("title").innerText = fruit.name[0]
+    loadquestions()
 }
 
+function loadquestions(){
 
+    document.getElementById("question-title").innerText = subjects[vraagcounter].title
+    document.getElementById("question-element").innerText = subjects[vraagcounter].statement
+
+    document.getElementById("btn-agree").onclick = function(){saveanswer("agree")};
+    document.getElementById("btn-none").onclick = function(){saveanswer("none")};
+    document.getElementById("btn-disagree").onclick = function(){saveanswer("disagree")};
+
+}
+
+function saveanswer(answer){
+    savedAnswers[vraagcounter] = answer
+    console.log(savedAnswers)
+
+
+
+
+    vraagcounter++
+    loadquestions()
+
+
+}
 
 
 
