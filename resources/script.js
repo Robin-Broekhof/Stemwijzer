6acda9b63d23f30282f7cacc1d0e8043c86abbd7
Ye Xiu <99059076@mydavinci.nl>
2021-03-09 10:24:23 +0100

    styling verbetert
    styling van vragen pagina verbetert

--- resources/script.js
+++ resources/script.js
@@ -24,9 +24,9 @@ function loadquestions(){   /**loads in the question and keeps loading next ques
     document.getElementById("question-title").innerText = subjects[vraagcounter].title
     document.getElementById("question-element").innerText = subjects[vraagcounter].statement
 
-    document.getElementById("btn-agree").onclick = function(){saveanswer("agree")};
+    document.getElementById("btn-agree").onclick = function(){saveanswer("pro")};
     document.getElementById("btn-none").onclick = function(){saveanswer("none")};
-    document.getElementById("btn-disagree").onclick = function(){saveanswer("disagree")};
+    document.getElementById("btn-disagree").onclick = function(){saveanswer("contra")};
     document.getElementById("btn-skip").onclick = function(){saveanswer("skipped")};
 
     document.getElementById("back").onclick = questionback
@@ -35,21 +35,20 @@ function loadquestions(){   /**loads in the question and keeps loading next ques
 function saveanswer(answer){   /**  saves the answer into a variable */
     savedAnswers[vraagcounter] = answer
     console.log(savedAnswers)
-    console.log (subjects.length)
-
-
     vraagcounter++
-
     if(subjects.length == savedAnswers.length){
-        alert("je bent klaar")
-        /** !!!!nieuwe functie voor dom en end pagina !!!!*/
-        document.getElementById("homepage-content").style.display = "none"
-        document.getElementById("onclick-button").style.display = "none"
-        document.getElementById("questions-content").style.display = "none"
+        endresult()
     }
     else{
         loadquestions()
     }
+}
+
+function endresult(){
+    document.getElementById("homepage-content").style.display = "none"
+    document.getElementById("onclick-button").style.display = "none"
+    document.getElementById("questions-content").style.display = "none"
+    alert("je bent klaar")
 }
 
 
@@ -63,17 +62,16 @@ function questionback(){    /** functionality of the back button */
     }
 }
 
-
+funtion remember-question(){
+        var button-clicked
+}
 
 /**
 
-
+vragen onthouden als je terug gaat en dan zie je vorig antwoord in blauw op de knop
 
 console.log (subjects[vraagnummer].statement)
 console.log (subjects[vraagnummer].parties[1].position)
-
-
-
 
  */
 
@@ -83,5 +81,7 @@ console.log (subjects[vraagnummer].parties[1].position)
 
 
 
-
+questionsdom()
+/** 
 homepage()
+*/
